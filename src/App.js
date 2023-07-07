011266b21ef98ca7ec305de77a61a7a943fbaefd
DuyDangCode <21520774@gm.uit.edu.vn>
2023-07-07 16:50:18 +0700

    modify search result

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import Data from "./mock-data.json";
+import Data from "./getData";
+import { useState } from "react";
+
+let suggest = Data;
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [btn, setBtn] = useState(false);
+  const [propose, setPropose] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container" style={{ width: "max-content" }}>
+      <div class="container" style={{ width: "100%" }}>
+        <div style={{ flexDirection: "row" }}>
+          <input
+            placeholder="search with Elasticsearch"
+            class="search"
+            value={query}
+            onChange={(event) => {
+              //call search function
+              setQuery(event.target.value);
+              setBtn(false);
+              suggest = Data;
+            }}
+          />
+          <button
+            class="btn"
+            onClick={() => {
+              setBtn(true);
+              suggest = null;
+            }}
+          >
+            Enter
+          </button>
+        </div>
+
+        {btn
+          ? Data.filter((movie) => {
+              if (
+                query !== "" &&
+                movie.title.toLowerCase().includes(query.toLowerCase())
+              ) {
+                return movie;
+              }
+            }).map((movie, index) => {
+              return (
+                <div key={index} class="box">
+                  <p>{movie.title}</p>
+                </div>
+              );
+            })
+          : null}
+      </div>
+
+      <div style={{ width: "100%" }}>
+        {console.log(1)}
+        {suggest != null
+          ? suggest
+              .filter((movie) => {
+                if (
+                  query !== "" &&
+                  movie.title.toLowerCase().includes(query.toLowerCase())
+                ) {
+                  return movie;
+                }
+              })
+              .map((movie, index) => {
+                return (
+                  <div
+                    key={index}
+                    class="propose"
+                    onClick={() => {
+                      setQuery(movie.title);
+                      setBtn(true);
+                      suggest = null;
+                    }}
+                  >
+                    <p>{movie.title}</p>
+                  </div>
+                );
+              })
+          : null}
+      </div>
     </div>
   );
 }
---
+++ src/getData.js
@@ -0,0 +1,18 @@
+const example = [
+  { id: 1, title: "Kiki's Delivery Service" },
+  { id: 2, title: "Quiet City" },
+  { id: 3, title: "Teen Wolf Too" },
+  { id: 4, title: "Our Idiot Brother" },
+  { id: 5, title: "Battle of Britain" },
+  { id: 6, title: "Centennial" },
+  { id: 7, title: "Crime on the Highway" },
+  { id: 8, title: "The Periwig-Maker" },
+  { id: 9, title: "Blame it on Fidel! (La faute à Fidel!)" },
+  { id: 10, title: "Pete Smalls Is Dead" },
+];
+
+function getData() {
+  return example;
+}
+
+export default getData();
